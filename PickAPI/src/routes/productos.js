01b5644833b64992ae5f549d95c55eac79605327
juana <valentinamoreno021217@gmail.com>
2024-05-13 11:25:02 -0500

    Solicitud HTTP PUT
    Maneja las solicitudes PUT para actualizar la información de un producto en la base de datos utilizando un esquema de Mongoose.

--- PickAPI/src/routes/productos.js
+++ PickAPI/src/routes/productos.js
@@ -9,3 +9,14 @@ router.post("/products", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 module.exports = router;
+
+router.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre_producto, descripcion, precio, proveedor, cantidad } = req.body;
+    productoSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre_producto, descripcion, precio, proveedor, cantidad }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
