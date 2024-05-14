6d771e3834f4a88f4a96b6719e9714197cb5784e
Nata0524 <141965549+Nata0524@users.noreply.github.com>
2024-05-13 19:20:35 -0500

    SOLICITUD DELETE
    Código que maneja una solicitud DELETE a la ruta /products/:id, donde :id es el ID del producto que se desea eliminar.

--- PickAPI/src/routes/productos.js
+++ PickAPI/src/routes/productos.js
@@ -1,22 +1,11 @@
-const express = require("express");
-const router = express.Router(); //manejador de rutas de express
-const productosSchema = require("../models/productos");
-router.post("/products", (req, res) => {
-    const productos = productosSchema(req.body);
-    productos
-        .save()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
-module.exports = router;
-
-router.put("/products/:id", (req, res) => {
-    const { id } = req.params;
-    const { nombre_producto, descripcion, precio, proveedor, cantidad } = req.body;
-    productoSchema
-        .updateOne({ _id: id }, {
-            $set: { nombre_producto, descripcion, precio, proveedor, cantidad }
-        })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
+router.delete("/products/:id", (req, res) => {
+ const { id } = req.params;
+     productoSchema
+     .findByIdAndDelete(id)
+     .then((data) => {
+     res.json(data);
+     })
+     .catch((error) => {
+    res.json({ message: error });
+ });
+    });
