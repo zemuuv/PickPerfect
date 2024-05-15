194787f6e2e636beb73ee7b254cea69170adf322
Santiago Lozano <santiagolozanoorrego@hotmail.com>
2024-05-14 20:45:05 -0500

    peitición get productos
    se hizo la petición get en la clase productos.js

--- PickAPI/src/routes/productos.js
+++ PickAPI/src/routes/productos.js
@@ -29,4 +29,9 @@ router.delete("/products/:id", (req, res) => {
             res.json({ message: error });
         });
 });
+router.get("/products", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 module.exports = router;
