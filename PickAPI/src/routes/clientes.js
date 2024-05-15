36f60b3657dd7171614185b8077d6c6beafaf321
Santiago Lozano <santiagolozanoorrego@hotmail.com>
2024-05-14 20:44:15 -0500

    petición get
    Se hizo la peitición get en clientes.js

--- PickAPI/src/routes/clientes.js
+++ PickAPI/src/routes/clientes.js
@@ -29,4 +29,9 @@ router.delete("/clientes/:id", (req, res) => {
             res.json({ message: error });
         });
 });
+router.get("/clientes", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 module.exports = router;
